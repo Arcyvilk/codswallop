285984a0e20c8031cb6c693e7c9cc961b5857715
Arcyvilk <arcyvilk@pm.me>
2026-06-02 23:47:08 +0200

    refactor: move storybook example components to dedicated dirs

--- client/src/components/Button/Button.svelte
+++ client/src/components/Button/Button.svelte
@@ -2,15 +2,10 @@
   import './button.css';
 
   interface Props {
-    /** Is this the principal call to action on the page? */
     primary?: boolean;
-    /** What background color to use */
     backgroundColor?: string;
-    /** How large should the button be? */
     size?: 'small' | 'medium' | 'large';
-    /** Button contents */
     label: string;
-    /** The onclick event handler */
     onclick?: () => void;
   }
 
--- client/src/components/Page/Page.stories.svelte
+++ client/src/components/Page/Page.stories.svelte
@@ -4,12 +4,10 @@
   import Page from './Page.svelte';
   import { fn } from 'storybook/test';
 
-  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
   const { Story } = defineMeta({
     title: 'Example/Page',
     component: Page,
     parameters: {
-      // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
       layout: 'fullscreen',
     },
   });
